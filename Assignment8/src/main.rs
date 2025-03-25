988c2570416994acd7e4088eaf61885596bd547b
leander <lflopez@ualberta.ca>
2025-03-24 19:10:43 -0600

    lol

---
+++ Assignment8/src/main.rs
@@ -0,0 +1,60 @@
+use std::io;
+use tokio::time::{sleep, Duration};
+use chrono::Utc;
+use std::collections::HashMap;
+
+async fn handle_cmd(cmd: char) {
+    match cmd {
+        'i' => {
+            if let Ok(r) = reqwest::get("https://httpbin.org/ip").await {
+                if let Ok(ip) = r.json::<HashMap<String, String>>().await {
+                    println!("{:?}", ip);
+                }
+            }
+        }
+        'd' => {
+            if let Ok(r) = reqwest::get("https://httpbin.org/delay/9").await {
+                println!("{:#?}", r);
+            }
+        }
+        other => println!("unknown cmd: {}", other),
+    }
+}
+
+async fn read_cmd() -> Result<usize, Box<dyn std::error::Error>> {
+    let mut data = String::new();
+    let stdin = io::stdin();
+    match stdin.read_line(&mut data) {
+        Ok(len) => {
+            if len > 0 {
+                if let Some(cmd) = data.chars().next() {
+                    handle_cmd(cmd).await;
+                }
+            }
+            Ok(len)
+        }
+        Err(e) => Err(Box::new(e)),
+    }
+}
+
+async fn periodic_task(interval: u64) {
+    sleep(Duration::from_secs(interval)).await;
+    println!("UTC now: {}", Utc::now());
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    loop {
+        match read_cmd().await {
+            Ok(len) => {
+                if len == 0 {
+                    break;
+                }
+            }
+            Err(e) => println!("input error {}", e),
+        }
+        periodic_task(5).await;
+    }
+
+    Ok(())
+}
--- assignment7/README.md
+++
@@ -1,8 +0,0 @@
-# ECE 421 Assignment 7
-## Question 1
-in directory Bank_Application
-## Question 2
-### Part A
-The move keyword in thread:spawn causes variabled accessed in the closure arguement to be consumed by the thread. This prevents the same variable existing into closures which could cause memory leaks. To make it work, you should wrap sample data in Arc and then clone it in the same closure where the thread is spawned.
-### Part B
-in directory Assignment7_Q2
